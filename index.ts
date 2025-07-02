e052d2687301efea8cb28207abf4b45a78a74322
GhostGamer6969 <arambhmani@hotmail.com>
2025-07-02 23:41:42 +0530

    minting using gill

--- index.ts
+++ index.ts
@@ -1,11 +1,14 @@
+
 // import { createTransaction, createSolanaClient } from "gill";
 import { loadKeypairSignerFromFile } from "gill/node";
-import { getAddMemoInstruction } from "gill/programs";
+import { getAddMemoInstruction, getAssociatedTokenAccountAddress, getCreateNativeMintInstruction, getCreateTokenInstructions, getMintTokensInstructions, getTokenMetadataAddress } from "gill/programs";
 import {
     createTransaction,
     createSolanaClient,
     signTransactionMessageWithSigners,
     getSignatureFromTransaction,
+    generateKeyPairSigner,
+    address,
 } from "gill";
 
 /**
@@ -18,17 +21,44 @@ const { rpc, sendAndConfirmTransaction } = createSolanaClient({
     urlOrMoniker: "devnet",
 });
 const { value: latestBlockhash } = await rpc.getLatestBlockhash().send();
+// const mint = await generateKeyPairSigner()
+const mint = address("6QRY8Stw4VzgGSLeTavBNAcpVNHV2ArFnpg7GKQx5vrL");
+const destination = address("DWyWmTCLqfLAzfeiaDZmxVa2Y8qWaehYyHsiFtpPNfND");
+// const instructions = getCreateTokenInstructions(
+//     {
+//         decimals: 6,
+//         mintAuthority: signer,
+//         updateAuthority: signer,
+//         metadata: {
+//             name: "ZORO",
+//             symbol: "ZORO",
+//             uri: "https://static.vecteezy.com/system/resources/previews/033/530/421/non_2x/roronoa-zoro-onepiece-icon-free-png.png",
+//             isMutable: false
+//         },
+//         feePayer: signer,
+//         mint: mint,
+//         metadataAddress: await getTokenMetadataAddress(mint.address)
+//     }
+// );
+
+const mintInstrction = getMintTokensInstructions({
+    mint,
+    feePayer: signer,
+    mintAuthority: signer,
+    destination: destination,
+    ata: await getAssociatedTokenAccountAddress(mint, destination),
+    amount: 100_000_000_000_000
+})
 
 const transaction = createTransaction({
     version: "legacy", // or `0` if using address lookup tables
     feePayer: signer,
     instructions: [
-        getAddMemoInstruction({
-            memo: "gm world!",
-        }),
+        ...mintInstrction, // the array from getMintTokensInstructions
+        getAddMemoInstruction({ memo: "100M token minted" }),
     ],
     latestBlockhash,
-    // computeUnitLimit, // optional, but highly recommend to set
+    // computeUnitLimit, // optional, but highly5 recommend to set
     // computeUnitPrice, // optional, but highly recommend to set
 });
 
